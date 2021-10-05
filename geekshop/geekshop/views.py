f802b4ee08339fb96724db18a8ef2f9b9af7ab55
Ilia Marchukov <Gerald16311@gmail.com>
2021-10-05 10:57:35 +0400

    first lesson

---
+++ geekshop/geekshop/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def main(request):
+    return render(request, 'geekshop/index.html')
+
+
+def contact(request):
+    return render(request, 'geekshop/contact.html')
+
+
